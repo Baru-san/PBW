c92292ac0cd7d4c0cc4dc23aea90434209988988
ihsan <101538712+Baru-san@users.noreply.github.com>
2023-02-14 14:09:30 +0700

    feat: add tugas 1

---
+++ Latihan01.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Latihan01</title>
+    <script>
+        console.log("Kalkulator sederhana");
+    </script>
+    <style>
+        .sisi {
+        height: 100%;
+        width: 50%;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        overflow-x: hidden;
+        padding-top: 20px;
+        }
+        .kiri {
+            left: 0;
+            background-color: #3AB0FF;
+
+        }
+        .kanan {
+            right: 0;
+            background-color: #F87474;
+            border-left: 1px solid black;
+        }
+        ul {
+            text-align: left;
+        }
+        .centered {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        background-color: #F9F2ED;   
+        }
+        .kotakAngka {
+            width: 60px;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="sisi kiri">
+        <div class="centered">
+            <h1>Biodata</h1>
+            <ul>
+                <li><pre>Nama Lengkap : Muhammad Akbarul ihsan</pre></li>
+                <li><pre>NIM          : 2108107010044</pre></li>
+                <li><pre>Jurusan      : Informatika</pre></li>
+                <li><pre>Fakultas     : MIPA</pre></li>
+                <li><pre>Universitas  : Universitas Syiah Kuala  </pre></li>
+                <li><pre>alamat       : ulee kareng</pre></li>
+                <li><pre>Foto</li>
+            </ul>
+            <img class="foto" 
+            src="https://cdn.idntimes.com/content-images/duniaku/post/20230206/fobysqvayai713n-4fc29fdb249be5a1125f894e0ae8fd84.jpg"
+            width="100" height="100"
+            >
+            <br><br>
+
+        </div>
+    </div>
+    <div class="sisi kanan">
+        <div class="centered">
+            
+                <h1>&emsp;Kalkulator&emsp;</h1>
+                
+                <input class="kotakAngka" type="number" id="nilai1" placeholder="Nilai 1" disabled="true"> <br>
+                <input class="kotakAngka" type="number" id="nilai2" placeholder="Nilai 2" disabled="true">
+                <br><br>
+                <input type="button" id="butNilai1" value="Nilai 1" onclick="document.getElementById('nilai1').value+=assignNilai1()">
+                <input type="button" id="butNilai2" value="Nilai 2" onclick="document.getElementById('nilai2').value+=assignNilai2()">
+                <br><br>
+                <label for="operasi">Pilih Operasi : </label>
+                    <select name="Operasi" id="Operasi">
+                        <option value="0">--operasi--</option>
+                        <option value="1">Penjumalahan</option>
+                        <option value="2">Pengurangan</option>
+                        <option value="3">Perkalian</option>
+                        <option value="4">Pembagian</option>
+                    </select>
+                <br>
+                <input type="button" id="btnHitung" value="proses" onclick="document.getElementById('hasil').value+=pilihOperasi()" >
+                <br>
+                <p>&emsp;Hasil : 
+                <input class="kotakAngka" type="number" id="hasil" disabled="true"> </p>
+
+            <script >
+                function assignNilai1(){
+                    var input = parseFloat(prompt("Enter the first number:"));
+                    var button = document.getElementById("butNilai1");
+                    button.disabled = true;
+                    return input;
+                }
+                function assignNilai2(){
+                    var input = parseFloat(prompt("Enter the Second number:"));
+                    var button = document.getElementById("butNilai2");
+                    button.disabled = true;
+                    return input;
+                }
+                function add(a, b) {
+                    var a1 = parseFloat(a);
+                    var b1 = parseFloat(b);
+                    var c = a1+b1;
+                    return c;
+                }
+                function kurang(a, b) {
+                    var angka1 = parseFloat(a);
+                    var angka2 = parseFloat(b);
+                    var hasil = angka1-angka2;
+                    return hasil;
+                }
+                function multiplication(a, b) {
+                    var angka1 = parseFloat(a);
+                    var angka2 = parseFloat(b);
+                    var hasil = angka1*angka2;
+                    return hasil;
+                }
+                function bagi(a, b) {
+                    var angka1 = parseFloat(a);
+                    var angka2 = parseFloat(b);
+                    var hasil = angka1/angka2;
+                    return hasil;
+                }
+
+                function pilihOperasi(){
+                    var x = document.getElementById("Operasi");
+                    var operasipilihan = parseInt(x.options[x.selectedIndex].value);
+                    var button = document.getElementById("btnHitung")
+                    button.disabled = true;
+                    switch (operasipilihan){
+                        case 1:
+                            return add(document.getElementById('nilai1').value, document.getElementById('nilai2').value);
+                            break;
+                        case 2:
+                            return kurang(document.getElementById('nilai1').value, document.getElementById('nilai2').value);
+                            break;
+                        case 3:
+                            return multiplication(document.getElementById('nilai1').value, document.getElementById('nilai2').value);
+                            break;
+                        case 4:
+                            return bagi(document.getElementById('nilai1').value, document.getElementById('nilai2').value);
+                            break;
+                    }
+
+                }
+            </script>
+        </div>
+    </div>
+
+
+</body>
+</html>
